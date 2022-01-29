592909f82c206e653f113808bd8b1ad357e73bee
Ulugbek01 <xyz207820@gmail.com>
2022-01-29 11:57:08 +0500

    complex styled-components

--- src/components/FooterStyle.js
+++ src/components/FooterStyle.js
@@ -1,29 +1,54 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const getSize = (size) => {
-  let res = null;
-  if (size > 10) {
-    res = 30;
-  } else {
-    res = 8;
+  switch (size) {
+    case "small":
+      return "14px";
+    case "medium":
+      return "20px";
+    case "large":
+      return "28px";
+    default:
+      return "32px";
   }
-
-  return res + "px";
 };
 
+const buttonSize = (size) => {
+  switch (size) {
+    case "small":
+      return "200px";
+    case "medium":
+      return "320px";
+    case "large":
+      return "400px";
+    default:
+      return "150px";
+  }
+};
+
+const primaryColor = css`
+  color: #d63384;
+  text-shadow: 1px 2px 3px #666;
+  font-weight: 600;
+`;
+
 export const Container = styled.div`
-  /* background-color: blue; */
+  background-color: ${({ para }) => (para ? "white" : "black")};
   padding: 20px;
-  color: #333;
+  color: ${({ para }) => (para ? "blcak" : "white")};
   font-size: ${({ size }) => getSize(size)};
   font-weight: 600;
+  height: 100vh;
 `;
 export const Button = styled.button`
   padding: 10px 20px;
   background-color: ${({ active }) => (active === "true" ? "blue" : "red")};
-  color: #fff;
+  color: ${primaryColor};
   outline: none;
   cursor: pointer;
+  font-size: ${({ size }) => getSize(size)};
+  width: ${({ size }) => buttonSize(size)};
+  width: ${buttonSize};
 
   @media (max-width: 1000px) {
     font-size: 24px;
@@ -39,3 +64,9 @@ export const Button = styled.button`
     transition: all 0.3s;
   }
 `;
+
+// Container.Box = styled.div`
+//   width: 400px;
+//   height: 100px;
+//   background-color: blue;
+// `;
