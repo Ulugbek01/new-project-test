0f993bd1aa9a0be3d55bb36560b42c438569fd23
Ulugbek01 <xyz207820@gmail.com>
2022-01-29 23:56:08 +0500

    Buyurtmalar add function

--- src/components/Mahsulotlar/index.jsx
+++ src/components/Mahsulotlar/index.jsx
@@ -2,24 +2,49 @@ import React, { Component } from 'react';
 import CardItem from './CardItem';
 import { list } from '../../utils/users';
 import { Container } from './style';
-
-
 export default class index extends Component {
     state = {
         users: list,
+        name:'',
+        surname: '',
+        title:'',
     }
   render() {
       const onDelete =(id)=> {
         let newData = this.state.users.filter((value) => value.id !== id);
           this.setState({users: newData});
-          console.log(id, " id")
         }
-
+      
+      const onChange = (e)=> {
+        // console.log(e.target.value);
+        this.setState({[e.target.name]: e.target.value});
+      }
+      
+      const onAdd = ()=> {
+        let newUser = {
+          id: this.state.users.length + 1,
+          title: this.state.title,
+          name: this.state.name,
+          surname: this.state.surname
+        }
+        // console.log(newUser);
+        this.setState({users: [...this.state.users, newUser]});
+      }
     return (
         <Container>
-            {this.state.users.map((value)=> 
-                <CardItem value={value} key={value.id} onDelete={onDelete}/>
-            )}
+            <div>
+              <h3>Name: {this.state.name}</h3>
+              <h3>Surname: {this.state.surname}</h3>
+              <h3>Title: {this.state.title}</h3>
+              <input name='name' onChange={onChange} className='input' type="text" placeholder='name' />
+              <input name='surname' onChange={onChange} className='input' type="text" placeholder='surname' />
+              <input name='title' onChange={onChange} className='input' type="text" placeholder='title' />
+              <button className='add-btn' onClick={onAdd}>add</button>
+            </div>
+            {this.state.users.length > 0 ?
+                this.state.users.map((value)=> 
+                    <CardItem value={value} key={value.id} onDelete={onDelete}/>
+                ) : <h1>No data</h1>}
         </Container>
     )
   }
--- src/components/Mahsulotlar/style.js
+++ src/components/Mahsulotlar/style.js
@@ -3,4 +3,19 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color: #e5e5e5;
   height: 100vh;
+
+  .input {
+    outline: none;
+  }
+
+  .add-btn {
+    background-color: #0e5dde;
+    padding: 5px 20px;
+    color: #fff;
+    cursor: pointer;
+    margin-left: 20px;
+    outline: none;
+    /* border: none; */
+    border-radius: 4px;
+  }
 `;
